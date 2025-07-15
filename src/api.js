d5ccae6cc8e9237510215fe25c8c75c34b6163df
AlifAbhipraya <alifelangabhipraya@gmail.com>
2025-07-15 11:51:39 +0700

    api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080', // Change this to your backend URL
+  baseURL: 'https://api.boss-team.site', // Change this to your backend URL
 });
 
 export const fetchMovies = (params) => API.get('/movies', { params });
